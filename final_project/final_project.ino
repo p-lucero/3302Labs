0a7b1b415f57c3727e64768886202ab78bf270e1
p-lucero <p-lucero@users.noreply.github.com>
2018-11-26 16:52:29 -0700

    Very basic scaffolding

--- final_project/final_project.ino
+++ final_project/final_project.ino
@@ -1,16 +1,67 @@
-void setup() {
-  // put your setup code here, to run once:
+// This snippet preserves the include of the sparki header file under Sparkiduino
+// while also allowing for easier debugging without a Sparki present
+// To make use of this, compile with -DSPARKI_HEADLESS_DEBUGGING in your g++ flags
+#ifdef SPARKI_HEADLESS_DEBUGGING
+#include "sparki.h"
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+#else
+#include <sparki.h>
+#define M_PI 3.14159
+#endif
 
+// State machine defines
+#define PATH_PLANNING 0
+#define PATH_FOLLOWING 1
+#define WRONG_FLOOR 2
+#define IN_ELEVATOR 3
+#define FOUND_OBJECT 4
+#define CARRY_PERSON 5
+
+// Robot physical constants
+#define ROBOT_SPEED 0.0278
+#define CYCLE_TIME .100
+#define AXLE_DIAMETER 0.0865
+#define WHEEL_RADIUS 0.03
+#define MAP_SIZE_X // TODO
+#define MAP_SIZE_Y // TODO
+// as a special variable to improve readability; may improve performance as a const too
+const short CYCLE_TIME_MS = CYCLE_TIME * 1000;
+
+// Robot digital constants
+#define FWD 1
+#define NONE 0
+#define BCK -1
+#define NUM_X_CELLS 6
+#define NUM_Y_CELLS 6
+#define NUM_FLOORS 2 // FIXME
+#define BIG_NUMBER 255
+
+// state machine control variables; prefer byte over int, since it's more efficient
+byte current_state;
+
+// World map
+byte world_map[NUM_X_CELLS][NUM_Y_CELLS][NUM_FLOORS];
+
+// IK and odometry variables
+float pose_x = 0., pose_y = 0., pose_theta = 0.;
+float dest_pose_x = 0., dest_pose_y = 0., dest_pose_theta = 0.;
+float d_err = 0., b_err =  0., h_err = 0., phi_l = 0., phi_r = 0.;
+float left_speed_pct = 0., right_speed_pct = 0.;
+byte left_dir, right_dir, left_wheel_rotating = NONE, right_wheel_rotating = NONE;
+byte pose_floor = 0, dest_floor;
+
+void setup() {
+  // TODO set up the world map here
   // current_state = PATH_PLANNING
   // goal_state = person_to_rescue
-
 }
 
-
-
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Clear the LCD of whatever was present last cycle
+  // Set up bookkeeping variables; add whatever you need to this set of declarations
+  sparki.clearLCD();
+  unsigned long begin_time = millis(), end_time;
 
   switch (current_state){
     case PATH_PLANNING:
@@ -108,7 +159,18 @@ void loop() {
      * current_state = PATH_PLANNING
      * 
      */
+     
+     break;
     
   }
 
+  // Always update the LCD: only call this here to avoid anomalous behavior
+  sparki.updateLCD();
+
+  // Check how long to delay, so that we take some time between cycles
+  end_time = millis();
+  if (end_time - begin_time < CYCLE_TIME_MS)
+    delay(CYCLE_TIME_MS - (end_time - begin_time));
+  else
+    delay(10);
 }
